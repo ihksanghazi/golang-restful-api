978f0437362656ccf7c0d6f64e81b25b776d8e43
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-07-09 19:30:15 +0700

    memisahkan router ke dalam file sendiri di folder app

--- main.go
+++ main.go
@@ -3,17 +3,14 @@ package main
 import (
 	"golang-restful-api/app"
 	"golang-restful-api/controller"
-	"golang-restful-api/exception"
 	"golang-restful-api/helper"
 	"golang-restful-api/middleware"
 	"golang-restful-api/repository"
 	"golang-restful-api/service"
 	"net/http"
 
-	_ "github.com/go-sql-driver/mysql"
-
 	"github.com/go-playground/validator/v10"
-	"github.com/julienschmidt/httprouter"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -23,16 +20,7 @@ func main() {
 	categoryRepository:= repository.NewCategoryRepository()
 	categoryService:=service.NewCategoryService(categoryRepository, DB, validate)
 	categoryController:=controller.NewCategoryController(categoryService)
-
-	router := httprouter.New()
-
-	router.GET("/api/categories",categoryController.FindAll)
-	router.GET("/api/categories/:categoryId",categoryController.FindById)
-	router.POST("/api/categories/",categoryController.Create)
-	router.PUT("/api/categories/:categoryId",categoryController.Update)
-	router.DELETE("/api/categories/:categoryId",categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router:= app.Router(categoryController)
 
 	server:= http.Server{
 		Addr: "localhost:3000",
