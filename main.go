ab6a4aef502670d2865fe4ff0f998aa28e40c57d
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-07-09 10:47:22 +0700

    include route in server

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"golang-restful-api/app"
 	"golang-restful-api/controller"
+	"golang-restful-api/helper"
 	"golang-restful-api/repository"
 	"golang-restful-api/service"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/julienschmidt/httprouter"
@@ -25,4 +29,12 @@ func main() {
 	router.POST("/api/categories/",categoryController.Create)
 	router.PUT("/api/categories/:categoryId",categoryController.Update)
 	router.DELETE("/api/categories/:categoryId",categoryController.Delete)
+
+	server:= http.Server{
+		Addr: "localhost:3000",
+		Handler: router,
+	}
+
+	err:=server.ListenAndServe()
+	helper.PanicIfError(err)
 }
